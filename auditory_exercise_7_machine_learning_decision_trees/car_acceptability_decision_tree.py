dc0ad77b09da41b4c3f0d15e4b9ddab85db26875
lihnida <lihnida.jovanova@yahoo.com>
2024-06-30 03:58:06 +0200

    initial commit

--- auditory_exercise_7_machine_learning_decision_trees/car_acceptability_decision_tree.py
+++ auditory_exercise_7_machine_learning_decision_trees/car_acceptability_decision_tree.py
@@ -8,24 +8,23 @@ def read_file(file_name):
         csv_reader = csv.reader(doc, delimiter=',')
         dataset = list(csv_reader)[1:]
 
-        return dataset
+    return dataset
 
 
-if __name__ == "__main__":
-    dataset = read_file("car.csv")
+if __name__ == '__main__':
+    dataset = read_file('car.csv')
 
     encoder = OrdinalEncoder()
     encoder.fit([row[:-1] for row in dataset])
 
-    train_sat = dataset[:int(0.7 * len(dataset))]
-    train_x = [row[:-1] for row in train_sat]
-    train_y = [row[-1] for row in train_sat]
+    train_set = dataset[:int(0.7 * len(dataset))]
+    train_x = [row[:-1] for row in train_set]
+    train_y = [row[-1] for row in train_set]
+    train_x = encoder.transform(train_x)
 
-    test_set = dataset[int(0.7 * len(dataset))]
+    test_set = dataset[int(0.7 * len(dataset)):]
     test_x = [row[:-1] for row in test_set]
     test_y = [row[-1] for row in test_set]
-
-    train_x = encoder.transform(train_x)
     test_x = encoder.transform(test_x)
 
     # classifier = DecisionTreeClassifier(criterion='entropy', max_depth=5, max_leaf_nodes=20, random_state=0)
@@ -35,7 +34,7 @@ if __name__ == "__main__":
     print(f"Depth: {classifier.get_depth()}")
     print(f"Number of leaves: {classifier.get_n_leaves()}")
 
-    accuracy = 0;
+    accuracy = 0
 
     for i in range(len(test_set)):
         predicted_class = classifier.predict([test_x[i]])[0]
@@ -73,7 +72,7 @@ if __name__ == "__main__":
 
     test_x_3 = list()
     for t in test_x:
-        row = [t[i] for range(len(t)) if i != least_important_feature]
+        row = [t[i] for i in range(len(t)) if i != least_important_feature]
         test_x_3.append(row)
 
     classifier_2 = DecisionTreeClassifier(criterion="entropy", random_state=0)
@@ -88,4 +87,26 @@ if __name__ == "__main__":
     print(f"Depth (without least important feature): {classifier_3.get_depth()}")
     print(f"Number of leaves (without least important feature): {classifier_3.get_n_leaves()}")
 
+    accuracy_2 = 0
 
+    for i in range(len(test_set)):
+        predicted_class = classifier_2.predict([test_x_2[i]])[0]
+        true_class = test_y[i]
+        if predicted_class == true_class:
+            accuracy_2 += 1
+
+    accuracy_2 = accuracy_2 / len(test_set)
+
+    print(f"Accuracy (without most important feature): {accuracy_2}")
+
+    accuracy_3 = 0
+
+    for i in range(len(test_set)):
+        predicted_class = classifier_3.predict([test_x_3[i]])[0]
+        true_class = test_y[i]
+        if predicted_class == true_class:
+            accuracy_3 += 1
+
+    accuracy_3 = accuracy_3 / len(test_set)
+
+    print(f"Accuracy (without least important feature): {accuracy_3}")
